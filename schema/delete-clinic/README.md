9d530e5d8cf315aff8fbef375403d3587e57c3f5
Oren <orengolan@gmail.com>
2017-05-24 07:29:35 -0700

    misc

---
+++ schema/delete-clinic/README.md
@@ -0,0 +1,10 @@
+## Example for adding struct and removing it using it's id
+
+Usage:
+```
+go run main.go
+go run main.go -id 9c2177ed-408c-11e7-af40-843a4b0f5a10
+```
+
+You should see nothing after the second command and the db should be empty
+
--- schema/delete-clinic/main.go
+++ schema/delete-clinic/main.go
@@ -1,4 +1,10 @@
-// usage: go run main.go -email baz@gmail.com -password 12345 -name "josh"
+// Example for adding struct and removing it using it's id
+
+// usage:
+// go run main.go
+// go run main.go -id 9c2177ed-408c-11e7-af40-843a4b0f5a10
+
+// You should see nothing after the second command and the db should be empty
 
 package main
 
@@ -45,7 +51,8 @@ func main() {
 	store := initializeAndOpenGraph(dbPath)
 
 	if *adminId != "" {
-		id := quad.StringToValue(*adminId)
+		id := quad.IRI(*adminId)
+
 		err := store.RemoveNode(store.ValueOf(id))
 
 		if err != nil {
