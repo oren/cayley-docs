326197bbf66bfd918d13e61cc1cc6cca3399319f
Oren <orengolan@gmail.com>
2017-05-23 22:26:23 -0700

    misc

---
+++ schema/delete-clinic/main.go
@@ -0,0 +1,114 @@
+// usage: go run main.go -email baz@gmail.com -password 12345 -name "josh"
+
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+
+	"github.com/cayleygraph/cayley"
+	"github.com/cayleygraph/cayley/graph"
+	_ "github.com/cayleygraph/cayley/graph/bolt"
+	"github.com/cayleygraph/cayley/quad"
+	"github.com/cayleygraph/cayley/schema"
+	uuid "github.com/satori/go.uuid"
+)
+
+var dbPath = "/tmp/db.boltdb"
+
+func init() {
+	schema.RegisterType("Admin", Admin{})
+	schema.GenerateID = func(_ interface{}) quad.Value {
+		return quad.IRI(uuid.NewV1().String())
+	}
+}
+
+type Admin struct {
+	Name           string `json:"name" quad:"name"`
+	Email          string `json:"email" quad:"email"`
+	HashedPassword string `json:"hashedPassword"  quad:"hashed_password"`
+	Password       string `quad:"-"`
+	LoggedIn       string `quad:"opt"`
+}
+
+func checkErr(err error) {
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func main() {
+	adminId := flag.String("id", "", "Admin's Id")
+	flag.Parse()
+
+	store := initializeAndOpenGraph(dbPath)
+
+	if *adminId != "" {
+		id := quad.StringToValue(*adminId)
+		err := store.RemoveNode(store.ValueOf(id))
+
+		if err != nil {
+			fmt.Println("Error removing the node", err)
+		}
+
+		printAllQuads(store)
+		return
+	}
+
+	err := Insert(store, Admin{
+		Name:           "josh",
+		Email:          "josh@test.com",
+		HashedPassword: "abc",
+	})
+
+	checkErr(err)
+
+	fmt.Println("Admin was created.")
+	printAllQuads(store)
+}
+
+// helper functions
+
+func initializeAndOpenGraph(dbFile string) *cayley.Handle {
+	graph.InitQuadStore("bolt", dbFile, nil)
+
+	// Open and use the database
+	store, err := cayley.NewGraph("bolt", dbFile, nil)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	return store
+}
+
+func Insert(store *cayley.Handle, o interface{}) error {
+	qw := graph.NewWriter(store)
+	defer qw.Close() // don't forget to close a writer; it has some internal buffering
+	_, err := schema.WriteAsQuads(qw, o)
+	return err
+}
+
+func printAllQuads(store *cayley.Handle) {
+	quads, err := readAllQuads(store)
+
+	if err != nil {
+		fmt.Println("error reading quads:", err)
+	}
+
+	for _, q := range quads {
+		fmt.Println(q)
+	}
+}
+
+func readAllQuads(store *cayley.Handle) ([]quad.Quad, error) {
+	var results []quad.Quad
+	it := store.QuadsAllIterator()
+	defer it.Close()
+
+	for it.Next() {
+		results = append(results, store.Quad(it.Result()))
+	}
+
+	return results, nil
+}
